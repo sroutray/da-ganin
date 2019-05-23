64154336d365f3a1959a5cd23126ee598f25a018
Sandeep <sandeeproutray1999@gmail.com>
2019-05-23 06:27:32 +0530

    minor fix

--- model.py
+++ model.py
@@ -10,7 +10,14 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(32, 48, 5)
         self.fc1_c = nn.Linear(4*4*48, 100)
         self.fc2_c = nn.Linear(100, 100)
+        self.fc3_c = nn.Linear(100, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 4*4*48)
+        x = F.relu(self.fc1_c(x))
+        x = F.relu(self.fc2_c(x))
+        x = self.fc3_c(x)
+
+        return x
